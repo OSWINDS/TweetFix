ab533b73d6febd7f8adf6e763c51624620f6cacb
Antigoni Founta <a.m.founta@gmail.com>
2016-09-19 02:39:30 +0300

    (very) primitive layout for cases - images only

--- assets/js/morris-conf.js
+++ assets/js/morris-conf.js
@@ -5,24 +5,39 @@ var Script = function () {
     $(function () {
       // data stolen from http://howmanyleft.co.uk/vehicle/jaguar_'e'_type
       var tax_data = [
-           {"period": "2011 Q3", "licensed": 3407, "sorned": 660},
-           {"period": "2011 Q2", "licensed": 3351, "sorned": 629},
-           {"period": "2011 Q1", "licensed": 3269, "sorned": 618},
-           {"period": "2010 Q4", "licensed": 3246, "sorned": 661},
-           {"period": "2009 Q4", "licensed": 3171, "sorned": 676},
-           {"period": "2008 Q4", "licensed": 3155, "sorned": 681},
-           {"period": "2007 Q4", "licensed": 3226, "sorned": 620},
-           {"period": "2006 Q4", "licensed": 3245, "sorned": null},
-           {"period": "2005 Q4", "licensed": 3289, "sorned": null}
+           {"period": "2011 Q3", "licensed": 3407},
+           {"period": "2011 Q2", "licensed": 3351},
+           {"period": "2011 Q1", "licensed": 3269},
+           {"period": "2010 Q4", "licensed": 3246},
+           {"period": "2009 Q4", "licensed": 3171},
+           {"period": "2008 Q4", "licensed": 3155},
+           {"period": "2007 Q4", "licensed": 3226},
+           {"period": "2006 Q4", "licensed": 3245},
+           {"period": "2005 Q4", "licensed": 3289}
       ];
+
+          //  {"period": "2011 Q3", "licensed": 3407, "sorned": 660},
+          //  {"period": "2011 Q2", "licensed": 3351, "sorned": 629},
+          //  {"period": "2011 Q1", "licensed": 3269, "sorned": 618},
+          //  {"period": "2010 Q4", "licensed": 3246, "sorned": 661},
+          //  {"period": "2009 Q4", "licensed": 3171, "sorned": 676},
+          //  {"period": "2008 Q4", "licensed": 3155, "sorned": 681},
+          //  {"period": "2007 Q4", "licensed": 3226, "sorned": 620},
+          //  {"period": "2006 Q4", "licensed": 3245, "sorned": null},
+          //  {"period": "2005 Q4", "licensed": 3289, "sorned": null}
+
       Morris.Line({
         element: 'hero-graph',
         data: tax_data,
         xkey: 'period',
-        ykeys: ['licensed', 'sorned'],
-        labels: ['Licensed', 'Off the road'],
-        lineColors:['#4ECDC4','#ed5565']
+        ykeys: ['licensed'],
+        labels: ['Licensed'],
+        lineColors: ['#4ECDC4']
       });
+
+      // ykeys: ['licensed', 'sorned'],
+      // labels: ['Licensed', 'Off the road'],
+      // lineColors:['#4ECDC4','#ed5565']
 
       Morris.Donut({
         element: 'hero-donut',
@@ -101,7 +116,3 @@ var Script = function () {
     });
 
 }();
-
-
-
-
